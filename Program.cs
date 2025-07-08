214bcf9a7863ff13041fc81396aa4bd23b70272e
Beck <Beck@GARDEN>
2025-07-08 11:31:43 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Create a console application in C# to store and print personal details of a person like name, age and address on console screen. Make use of appropriate variables.
+
+namespace Assignment_1._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Billy Windows";
+            uint age = 70;
+            string address = "1 Microsoft Way, Redmond, WA 98052";
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Address: " + address);
+        }
+    }
+}
